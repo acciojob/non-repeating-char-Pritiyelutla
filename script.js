a2c630d3157e4728ea35fe05e08162c15f6161de
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-05 19:22:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
+    // Write your code here
+    let size = 26;
+    let arr = [...Array(size)].fill(0);
+     
+    for(let i=0; i<str.length; i++)
+    {
+        let val = str.charCodeAt(i)-97;
+        arr[val] = arr[val]+1;
+    }
+
+    console.log(arr);
+
+    for(let i=0;i<str.length; i++)
+    {
+        let val = str.charCodeAt(i)-97;
+        if(arr[val] == 1)
+        {
+            return str[i];
+        }
+    }
+   }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
